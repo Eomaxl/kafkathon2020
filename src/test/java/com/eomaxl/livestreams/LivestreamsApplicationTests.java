f411baa55ec11745054ff03f1e7c83bff2ec7d0b
Eomaxl <souravsnigdh@gmail.com>
2024-12-30 23:37:40 +0530

    Initial Commit

---
+++ src/test/java/com/eomaxl/livestreams/LivestreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eomaxl.livestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
